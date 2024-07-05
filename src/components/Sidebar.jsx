91adf25ed47e9b5feaf008588f5d17caa3e9151d
jsprrnty <jasperjr.rantay@gmail.com>
2024-07-05 23:17:40 +0800

    Sytling Sidebar and adding react-icon package

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,23 @@
+import { FaAngleRight, FaRegSun   } from 'react-icons/fa';
+
 function Sidebar() {
 
     return (
         <>
             <section className="sidebar">
-                <div>
-                    <ul>
-                        <li><a href="#">Raw</a></li>
-                        <li><a href="#">Processed</a></li>
-                        <li><a href="#">For Production</a></li>
-                    </ul>
+                <div className="sidebar-container">
+                    <div className="sidebar-profile">
+                        <h2>Profile</h2>
+                        <p>Account Setting <span className='icon'>< FaRegSun /></span></p>
+                    </div>
+                    <div className="sidebar-items">
+                        <h2>Product</h2>
+                        <ul>
+                            <li className="sidebar-links"><a href="#">Raw <span className='icon'><FaAngleRight /></span></a></li>
+                            <li className="sidebar-links"><a href="#">Processed <span className='icon'><FaAngleRight /></span></a></li>
+                            <li className="sidebar-links"><a href="#">For Production <span className='icon'><FaAngleRight /></span></a></li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         </>
