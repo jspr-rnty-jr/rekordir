5d68c3ac8e233263fe24398f909deb8604be9f97
jsprrnty <jasperjr.rantay@gmail.com>
2024-07-08 18:04:00 +0800

    Created Card Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,18 @@
+import Card from '../components/ui/Card'
+
 const Main = () => {
     return ( 
         <main>
-            <div className="main-container">
+            <div className='main-header'>
                 <h1>Main</h1>
+            </div>
+            <div className="main-container">
+                < Card />
+                < Card />
+                < Card />
+                < Card />
+                < Card />
+                < Card />
             </div>
         </main>
     );
---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+    return ( 
+        <div className="card">
+            <div className="card-header">Card Header</div>
+            <div className="card-body">
+                Lorem ipsum dolor sit amet. Lorem. Lorem ipsum dolor sit amet.
+            </div>
+            <div className="card-footer">Footer</div>
+        </div>
+    );
+}
+export default Card;
